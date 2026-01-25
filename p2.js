d43c693f40d8447f6ab1376ebd331a457e9ed2e8
Sandhya Kuppili <sandhya84@gmail.com>
2026-01-25 15:32:25 +0530

    math

---
+++ p2.js
@@ -0,0 +1,8 @@
+console.log(Math.floor(2.9));
+console.log(Math.ceil(2.1));
+console.log(Math.max(2.3,2.4));
+console.log(Math.min(2.3,2.4));
+console.log(Math.random());
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(20));
