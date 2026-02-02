92067845be819858ee5b2795a53ab2799668c39f
Sandhya Kuppili <sandhya84@gmail.com>
2026-02-02 20:46:50 +0530

    strings in java script

---
+++ p3.js
@@ -0,0 +1,14 @@
+'heloow'+'everyone';
+console.log('heloow'+'everyone');
+console.log('10'+'20');
+tshirt=20;
+socks=20;
+total=tshirt+socks;
+console.log('Items:'+'   '+'$'+total);
+console.log("i'm learning java script");
+console.log('some\ntext');
+let name = "Sandhya";
+let age = 20;
+
+let msg = `My name is ${name} and I am ${age} years old`;
+console.log(msg);
